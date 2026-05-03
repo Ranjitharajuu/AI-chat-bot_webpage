f2b3df6df23dd9645871e4e5285f455ae1824dd5
Ranju <ranju@gmail.com>
2026-05-03 10:08:12 +0530

    render setup fixed

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const cors = require("cors");
 const OpenAI = require("openai");
 const multer = require("multer");
 require("dotenv").config();
-
+const path = require("path");
 const app = express();
 const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use(express.json());
+const path = require("path");
 
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
